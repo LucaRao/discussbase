4b8ea3aec351b55d46701143ae5da847f4d07bc6
LucaRao <56021673+LucaRao@users.noreply.github.com>
2022-07-06 10:58:19 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -29,16 +29,6 @@ export default function Home() {
 
           <div className='column'>
             <figure className='image is-64x64'>
-            <img className='is-rounded' src='/vercel.svg' alt='vercel logo' style={{'width': '64px', 'height': '64px'}} />
-            </figure>
-            <h3 className='mt-1 is-size-4'><a href='https://vercel.com/'> Vercel</a></h3>
-          <p>
-          Vercel是一个开放的无服务器平台，适用于静态和混合应用程序，可与您的内容、商业或数据库集成。
-            </p>
-          </div>
-
-          <div className='column'>
-            <figure className='image is-64x64'>
               <img className='is-rounded' src='https://camo.githubusercontent.com/92ec9eb7eeab7db4f5919e3205918918c42e6772562afb4112a2909c1aaaa875/68747470733a2f2f6173736574732e76657263656c2e636f6d2f696d6167652f75706c6f61642f76313630373535343338352f7265706f7369746f726965732f6e6578742d6a732f6e6578742d6c6f676f2e706e67' alt='nextjs logo' />
             </figure>
             <h3 className='mt-1 is-size-4'><a href='https://nextjs.org/'> Nextjs</a></h3>
